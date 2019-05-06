4463771d6149c561103d2fa49a71b60f490e20da
Marc Llahona <marcllahona@gmail.com>
2019-05-06 12:00:46 -0700

    Add app them

---
+++ src/lib/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  palette: {
+    primary: '#00C78B',
+    secondary: '#343332',
+    onPrimary: '#FFFFFF',
+    onSecondary: '#FFFFFF',
+    inverted: '#FFFFFF',
+    backgroundForm: '#F2f2f2',
+    textForm: '#666666',
+    border: '#F2f2f2',
+    error: '#ED4464'
+  }
+};
+
+export default theme;
