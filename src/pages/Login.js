7cc3db97a3589408b3c007279a58447eb4d40fb4
Marc Llahona <marcllahona@gmail.com>
2019-05-06 19:50:41 -0700

    Add redirection to Home if user has a valid JWT stored

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Mutation } from 'react-apollo';
+import { Redirect } from 'react-router-dom';
+import User from '../graphql/types/User';
 import TextField from '../components/inputs/TextField';
 import { invalidInputString } from '../lib/utils';
 import LOGIN_MUTATION from '../graphql/mutation/login';
@@ -25,59 +27,68 @@ function Login(props) {
 
   return (
     <S.Body>
-      <S.Container>
-        <S.Title>Welcome to Chatspace!</S.Title>
-        <S.Instructions>Please login to start</S.Instructions>
-        <Mutation
-          mutation={LOGIN_MUTATION}
-          variables={{ email, password }}
-          //We refetch user query to get most current value
-          refetchQueries={[{ query: ME_QUERY }]}
-          //We do not want to keep cache of email/password from the User
-          fetchPolicy={FETCH_POLICY.NO_CACHE}
-          onError={onError}
-          onCompleted={e => onCompleted(e.login)}
-        >
-          {(login, { loading }) => {
-            if (loading) return <p>Loading</p>;
-            return (
-              <S.Form
-                method="POST"
-                onSubmit={async e => {
-                  e.preventDefault();
-                  await login();
-                  setEmail('');
-                  setPassword('');
-                }}
+      <User>
+        {({ data, loading, error }) => {
+          if (loading) return <p>Loading</p>;
+          if (data) return <Redirect to={'/'} />;
+          return (
+            <S.Container>
+              <S.Title>Welcome to Chatspace!</S.Title>
+              <S.Instructions>Please login to start</S.Instructions>
+              <Mutation
+                mutation={LOGIN_MUTATION}
+                variables={{ email, password }}
+                //We refetch user query to get most current value
+                refetchQueries={[{ query: ME_QUERY }]}
+                //We do not want to keep cache of email/password from the User
+                fetchPolicy={FETCH_POLICY.NO_CACHE}
+                onError={onError}
+                onCompleted={e => onCompleted(e.login)}
               >
-                <TextField
-                  label="Email*"
-                  name="email"
-                  value={email}
-                  placeholder="Ex: joe@gmail.com"
-                  onChange={e => setEmail(e.target.value)}
-                />
-                <TextField
-                  label="Password*"
-                  name="password"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                  type="password"
-                />
-                <S.FormButton
-                  disabled={
-                    invalidInputString(email) || invalidInputString(password)
-                  }
-                >
-                  Login
-                </S.FormButton>
-              </S.Form>
-            );
-          }}
-        </Mutation>
-        <p>Don't have an account?</p>
-        <S.CustomLink to="/registration">Register</S.CustomLink>
-      </S.Container>
+                {(login, { loading }) => {
+                  if (loading) return <p>Loading</p>;
+                  return (
+                    <S.Form
+                      method="POST"
+                      onSubmit={async e => {
+                        e.preventDefault();
+                        await login();
+                        setEmail('');
+                        setPassword('');
+                      }}
+                    >
+                      <TextField
+                        label="Email*"
+                        name="email"
+                        value={email}
+                        placeholder="Ex: joe@gmail.com"
+                        onChange={e => setEmail(e.target.value)}
+                      />
+                      <TextField
+                        label="Password*"
+                        name="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
+                      />
+                      <S.FormButton
+                        disabled={
+                          invalidInputString(email) ||
+                          invalidInputString(password)
+                        }
+                      >
+                        Login
+                      </S.FormButton>
+                    </S.Form>
+                  );
+                }}
+              </Mutation>
+              <p>Don't have an account?</p>
+              <S.CustomLink to="/registration">Register</S.CustomLink>
+            </S.Container>
+          );
+        }}
+      </User>
     </S.Body>
   );
 }
